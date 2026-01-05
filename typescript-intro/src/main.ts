333ef0d852df9f5118ae31ed482ee053d0ad617b
Miguel Angel González <migueljabugo@gmail.com>
2026-01-05 13:30:21 +0100

    Decorators

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -6,7 +6,8 @@ import './style.css'
 //import './topics/04-homework-types';
 
 //import './topics/08-classes';
-import './topics/09-generics';
+//import './topics/09-generics';
+import './topics/10-decorators';
 
 let app = document.querySelector<HTMLDivElement>('#app')!;
 
