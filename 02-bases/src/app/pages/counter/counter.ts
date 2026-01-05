c30b57cac65afdc93fc0a7fffef9ba7b5a1b76a8
Miguel Angel González <migueljabugo@gmail.com>
2026-01-05 15:01:07 +0100

    Signals

--- 02-bases/src/app/pages/counter/counter.ts
+++ 02-bases/src/app/pages/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter.html',
@@ -7,12 +7,15 @@ import { Component } from "@angular/core";
 export class CounterComponent {
 
   counter = 0;
+  counterSignal = signal(0);
 
   increaseBy(value: number): void {
     this.counter += value;
+    this.counterSignal.update(current => current + value);
   }
 
   resetCounter(): void {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
