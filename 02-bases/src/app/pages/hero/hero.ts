2a2f4d0c6a805e03f6eeb088259ca9942c97cce3
Miguel Angel González <migueljabugo@gmail.com>
2026-01-08 23:17:14 +0100

    42. Señales computadas

--- 02-bases/src/app/pages/hero/hero.ts
+++ 02-bases/src/app/pages/hero/hero.ts
@@ -1,18 +1,24 @@
-import { Component, signal } from "@angular/core";
-
+import { Component, signal, computed } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
 
 @Component({
   templateUrl: './hero.html',
-  styleUrl: './hero.css'
+  styleUrl: './hero.css',
+  imports: [UpperCasePipe],
 })
 export class HeroComponent {
 
   name = signal('Ironman');
   age = signal(45);
 
-  getHeroDescription(): string {
-    return `${ this.name() } - ${ this.age() }`;
-  }
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`;
+    return description;
+  });
+
+  capitalizeName = computed(() => {
+    return this.name().toUpperCase();
+  });
 
   changeHero(): void {
     this.name.set('Spiderman');
