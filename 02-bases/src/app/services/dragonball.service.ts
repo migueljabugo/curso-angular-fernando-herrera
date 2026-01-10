29022e57eac0f1fa13e534d5e077362474066921
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-10 19:11:51 +0100

    61 Agregar efecto para guardar personajes en localStorage y contar caracteres

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, effect } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,11 @@ export class ServiceNameService {
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8500 }
   ]);
+
+  saveToLocalStorage = effect(() => {
+    console.log(`Character count ${this.characters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(newCharacter: Character) {
     this.characters.update(chars => [...chars, newCharacter]);
