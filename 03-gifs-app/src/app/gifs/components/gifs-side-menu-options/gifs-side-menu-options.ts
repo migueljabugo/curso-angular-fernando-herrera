87417b5b31aabca3a3ed5c37b90efc6026551fef
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-11 01:18:41 +0100

    Agregar menú lateral dinámico con opciones de navegación y estilos

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -1,9 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.html',
   styleUrl: './gifs-side-menu-options.css'
 })
-export class GifsSideMenuOptions { }
+export class GifsSideMenuOptions {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Most popular gifs',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Search for gifs',
+      route: '/dashboard/search'
+    },
+  ];
+
+
+ }
