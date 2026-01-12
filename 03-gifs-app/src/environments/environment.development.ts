7d7fc1a0c325a6ebbf42b5f2a61dda544fc62499
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-12 12:54:31 +0100

    Agregar giphyApiKey al entorno de desarrollo

--- 03-gifs-app/src/environments/environment.development.ts
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Find the best gifs here!'
+  companySlogan: 'Find the best gifs here!',
+  giphyApiKey: 'RFn6j18tn0v4X11kcYeIhFyE7xtu4fqg'
 
 };
