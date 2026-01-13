07c4b6e53b2a238f70dbc095d8320a3a638bcfbc
Miguel Angel González <migueljabugo@gmail.com>
2026-01-13 19:45:46 +0100

    98. Mejorar la referencia de tipo en scrollDivRef y agregar verificación en onScroll en TrendingPage

--- 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
+++ 03-gifs-app/src/app/gifs/pages/trending-page/trending-page.ts
@@ -29,13 +29,19 @@ export default class TrendingPage {
 
   gifService = inject(GifService);
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv');
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
   onScroll(event: Event){
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
 
-    console.log(scrollDiv);
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
 
+    //console.log({scrollTotal: scrollTop + clientHeight, scrollHeight});
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    console.log(isAtBottom)
   }
 
  }
