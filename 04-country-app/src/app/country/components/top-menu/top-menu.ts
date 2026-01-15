1b920fa76b2091bdb94514877c3b59fa6007e141
Miguel Angel González <migueljabugo@gmail.com>
2026-01-15 21:47:06 +0100

    110

---
+++ 04-country-app/src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'country-top-menu',
+  imports: [],
+  templateUrl: './top-menu.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopMenu { }
