3ed41f79e6edc612c8e59f66231643176850c7fe
Miguel Angel González <migueljabugo@gmail.com>
2026-01-15 21:11:28 +0100

    108. agregar CountryLayout y reestructurar rutas para la página por capital

--- 04-country-app/src/app/country/country.routes.ts
+++ 04-country-app/src/app/country/country.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from "@angular/router";
+import { CountryLayout } from './layout/country-layout/country-layout';
 import { ByCapital } from "./pages/by-capital-page/by-capital-page";
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapital
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapital
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   }
 ]
 
