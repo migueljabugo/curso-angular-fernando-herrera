8bfefa80c4991b9b47c3ad204ac5a13c3ef680e5
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-18 15:06:06 +0100

    Actualización de la ruta con la busqueda

--- 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
+++ 04-country-app/src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -3,7 +3,7 @@ import { SearchInput } from '../../components/search-input/search-input';
 import { CountryList } from '../../components/country-list/country-list';
 import { CountryService } from '../../services/country.service';
 import { firstValueFrom } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -16,6 +16,9 @@ export class ByCapital {
   countryService = inject(CountryService);
 
   activatedRoute = inject(ActivatedRoute);
+
+  router = inject(Router);
+
   queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
 
   query = signal(this.queryParam);
@@ -26,6 +29,12 @@ export class ByCapital {
     params: ()=> ({ query: this.query() }),
     loader: async({params}) => {
       if(!params.query) return [];
+
+      this.router.navigate(['/country/by-capital'], {
+        queryParams: {
+          query: params.query
+        }
+      });
 
       return await firstValueFrom(this.countryService.searchByCapital(params.query));
 
