21de5811642673f36e0f0499e3c5257b85e584bd
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-18 14:14:40 +0100

    138. cache de las respuestas del API

--- 04-country-app/src/app/country/services/country.service.ts
+++ 04-country-app/src/app/country/services/country.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, delay, map, of, throwError } from 'rxjs';
+import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
 
 const API_URL = 'https://restcountries.com/v3.1';
 
@@ -12,17 +13,23 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, Country[]>();
+
 
 
   searchByCapital( query: string) {
     query = query.toLocaleLowerCase();
-    console.log("Emitiendo valor",query);
 
-    return of([]);
+    if (this.queryCacheCapital.has(query)) {
+      return of(this.queryCacheCapital.get(query)!);
+    }
+
+    console.log('Obteniendo info', query);
 
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
       .pipe(
         map(CountryMapper.fromRestCountryArrayToCountryArray),
+        tap((coutries) => this.queryCacheCapital.set(query, coutries)),
         catchError(error => {
 
           return throwError(() => new Error('No se pudo obtener resultados'));
