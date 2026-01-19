748c424b62b9cbf7c43bb37c2720d0f8530f567f
Miguel Angel González <migueljabugo@gmail.com>
2026-01-19 19:58:01 +0100

    151. Trabajo con pipes: UpperCase, LowerCase y TitleCase

--- 05-pipes-app/src/app/pages/basic-page/basic-page.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, UpperCasePipe, TitleCasePipe } from '@angular/common'
+import { ChangeDetectionStrategy, Component, isSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPage { }
+export default class BasicPage {
+
+  nameLower = signal('miguel angel');
+  nameUpper = signal('MIGUEL ANGEL');
+  fullName = signal('MiGuEl aNgEl');
+
+
+
+
+
+}
