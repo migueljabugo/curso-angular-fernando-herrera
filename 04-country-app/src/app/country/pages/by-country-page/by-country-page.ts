63f5018fcf46e32cb249ba5110a9bf7216be03de
Miguel Angel González <migueljabugo@gmail.com>
2026-01-19 19:04:53 +0100

    143. Carga pais por el parametro de la query

--- 04-country-app/src/app/country/pages/by-country-page/by-country-page.ts
+++ 04-country-app/src/app/country/pages/by-country-page/by-country-page.ts
@@ -3,6 +3,7 @@ import { CountryList } from '../../components/country-list/country-list';
 import { SearchInput } from '../../components/search-input/search-input';
 import { CountryService } from '../../services/country.service';
 import { firstValueFrom } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-country-page',
@@ -14,15 +15,27 @@ import { firstValueFrom } from 'rxjs';
 export class ByCountryPage {
 
   countryService = inject(CountryService);
-  query = signal('');
+
+  activatedRoute = inject(ActivatedRoute);
+
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+  query = signal(this.queryParam);
 
   countryResource = resource({
     params: ()=> ({ query: this.query() }),
     loader: async({params}) => {
       if(!params.query) return [];
 
-      return await firstValueFrom(this.countryService.searchByCountry(params.query));
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: params.query
+        }
+      });
 
+      return await firstValueFrom(this.countryService.searchByCountry(params.query));
     },
   });
 
