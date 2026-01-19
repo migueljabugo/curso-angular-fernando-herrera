5b1a40e3af97ac3f9d142e351ae3f5f7536aa37d
Miguel Angel González <migueljabugo@gmail.com>
2026-01-19 20:09:57 +0100

    152. Pipes numéricos

--- 05-pipes-app/src/app/pages/number-page/number-page.ts
+++ 05-pipes-app/src/app/pages/number-page/number-page.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [
+    DecimalPipe,
+    PercentPipe,
+    CurrencyPipe
+  ],
   templateUrl: './number-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class NumberPage { }
+export default class NumberPage {
+
+  totalSells = signal(2446852.55687);
+  percent = signal(0.4856);
+ }
