070f07f2d98ed8d956222674555e149d7e82339a
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-20 11:45:30 +0100

    Establecemos el idioma de la aplicación y creamos el componente para a futuro cambiar el locale de la aplicación

--- 05-pipes-app/src/app/app.config.ts
+++ 05-pipes-app/src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
+
+import localeES from '@angular/common/locales/es';
+import localeEN from '@angular/common/locales/en';
+import localeFR from '@angular/common/locales/fr';
+
+registerLocaleData(localeES, 'es');
+registerLocaleData(localeEN, 'en');
+registerLocaleData(localeFR, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
   ]
 };
