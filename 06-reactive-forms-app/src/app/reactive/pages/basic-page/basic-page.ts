20a4dbb73e87867cfcf4f36f119bfe5925e8e3f9
Miguel Angel González <migueljabugo@gmail.com>
2026-01-22 17:48:00 +0100

    181. Inicio de formulario reactivo

--- 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,11 +1,22 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './basic-page.html',
 })
-export class BasicPage { }
+export class BasicPage {
+
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+
+  });
+
+ }
