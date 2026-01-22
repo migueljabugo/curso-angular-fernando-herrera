ad52fc1ade902c5f452aa5e869d74ff42e8cbc6e
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-22 14:57:47 +0100

    179. Creación de la estructura del proyecto y las rutas

---
+++ 06-reactive-forms-app/src/app/shared/components/side-menu/side-menu.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.html',
+})
+export class SideMenu {
+
+
+
+}
