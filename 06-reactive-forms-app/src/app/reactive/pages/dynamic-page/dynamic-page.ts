0a41314f04b4e54fef786c7df91f8ee186f8e4e1
Miguel Angel González <migueljabugo@gmail.com>
2026-01-22 18:59:02 +0100

    187. Mostrar una lista de campos dinámicos desde un array y mantener sus valores actualizados

--- 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -1,11 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Form, FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { minLength } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './dynamic-page.html',
 })
-export class DynamicPage { }
+export class DynamicPage {
+
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', Validators.required, Validators.minLength(3)],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Final Fantasy', Validators.required]
+    ],
+    Validators.minLength(3)
+  )
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+ }
