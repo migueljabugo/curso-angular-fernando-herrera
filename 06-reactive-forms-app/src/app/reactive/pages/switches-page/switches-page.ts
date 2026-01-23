7b88c9ce4b39ccb0784d73a1e5775dc9f88ed6da
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-23 08:27:16 +0100

    196. Control de componentes de formulario radio, check y toggle

--- 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.ts
+++ 06-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,11 +1,41 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule,
   ],
   templateUrl: './switches-page.html',
 })
-export class SwitchesPage { }
+export class SwitchesPage {
+
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm = this.fb.group({
+    gender: [null, Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+  onSubmint(){
+    this.myForm.markAllAsTouched();
+    console.log(this.myForm.value);
+  }
+}
