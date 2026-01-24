8fc5611f29e83c61ca95fc5fcefcc5dd99e0ea22
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-24 19:29:10 +0100

    203

--- 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
+++ 06-reactive-forms-app/src/app/country/pages/country-page/country-page.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './country-page.html',
 })
-export class CountryPage { }
+export class CountryPage {
+
+  fb = inject(FormBuilder);
+  CountryService = inject(CountryService);
+
+  regions = signal(this.CountryService.regions);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+
+ }
