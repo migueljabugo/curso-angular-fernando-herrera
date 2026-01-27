785e357721b0bd525094f220a20de3dcc2e3d251
Miguel Angel González <migueljabugo@gmail.com>
2026-01-27 17:09:26 +0100

    211. Creación del proyecto y estructura para los ciclos de vida

---
+++ 07-lifecycle-hooks/src/app/components/navbar/navbar.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './navbar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+
+    nav {
+      display: flex;
+      gap: 1rem;
+      justify-content: center;
+      align-items: center;
+    }
+    .active {
+      color: #341162;
+      font-weight: bold;
+    }
+
+  `
+})
+export class Navbar { }
