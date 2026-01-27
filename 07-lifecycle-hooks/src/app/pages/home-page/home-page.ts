e93e004c1a62425c10474ed4250c9a98b4c5a694
Miguel Angel González <migueljabugo@gmail.com>
2026-01-27 17:19:55 +0100

    212. Logs para ver el ciclo de vida

--- 07-lifecycle-hooks/src/app/pages/home-page/home-page.ts
+++ 07-lifecycle-hooks/src/app/pages/home-page/home-page.ts
@@ -1,9 +1,48 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+const log = (...messages: string[]) => {
+  console.log(`${messages[0]} %c${messages.slice(1).join(', ')}`,
+  'color: #bada55');
+};
+
 @Component({
   selector: 'app-home-page',
   imports: [],
   templateUrl: './home-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage { }
+export class HomePage {
+
+  constructor() {
+    log('Constructor');
+  }
+
+  ngOnInit() {
+    log('ngOnInit', "Runs once after Angular has initialized all the component's inputs.");
+  }
+
+  ngOnChanges() {
+    log('ngOnChanges', "Runs every time the component's inputs have changed.");
+  }
+
+  ngDoCheck() {
+    log('ngDoCheck', 'Runs every time this component is checked for changes');
+  }
+
+  ngAfterContentInit() {
+    log('ngAfterContentInit', "Runs once after the component's content has been initialized");
+  }
+
+  ngAfterContentChecked() {
+    log('ngAfterContentChecked', 'Runs every time this component content has been checked for changes.');
+  }
+
+  ngAfterViewInit() {
+    log('ngAfterViewInit', "Runs once after the component's view has been initialized");
+  }
+
+  ngAfterViewChecked() {
+    log('ngAfterViewChecked', "Runs every time the component's view has been checked for changes.");
+  }
+
+}
