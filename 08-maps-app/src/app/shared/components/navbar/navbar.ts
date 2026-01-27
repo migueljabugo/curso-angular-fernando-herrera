b3743c7ec761279276629fecb4076bc42bda5e05
Miguel Angel González <migueljabugo@gmail.com>
2026-01-27 18:29:39 +0100

    221. Navegación entre páginas, creando menú desde el app.routes.ts

--- 08-maps-app/src/app/shared/components/navbar/navbar.ts
+++ 08-maps-app/src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink
+  ],
   templateUrl: './navbar.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Navbar { }
+export class Navbar {
+
+  router = inject(Router);
+
+  routes = routes.map(route => ({
+    path: route.path,
+    title: `${route.title ?? 'Maps en Angular'}`
+  })).filter(route => route.path !== '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter( (event) => event instanceof NavigationEnd),
+    //tap((event) => {console.log({ event })}),
+    map( event => event.url),
+    map( url => routes.find(route => `/${route.path}` === url)?.title ?? 'Maps en Angular')
+  );
+
+
+ }
