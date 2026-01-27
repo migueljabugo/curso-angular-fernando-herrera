1b94f3f006c48f71db2a58f2b3ef70c1763962d0
Miguel Angel González <migueljabugo@gmail.com>
2026-01-27 18:14:52 +0100

    220. Creación de la estructura de páginas del proyecto

---
+++ 08-maps-app/src/app/pages/markers-page/markers-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-markers-page',
+  imports: [],
+  templateUrl: './markers-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MarkersPage { }
