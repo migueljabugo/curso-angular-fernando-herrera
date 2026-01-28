2bf0c03cf8a1a0d5a22fb975d7ba8ae6266ae6d2
Miguel Angel González <migueljabugo@gmail.com>
2026-01-29 00:29:10 +0100

    243. Tarjetas de producto con animación de entrada personalizada

--- 09-teslo-shop/src/app/store-front/components/product-card/product-card.ts
+++ 09-teslo-shop/src/app/store-front/components/product-card/product-card.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'product-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
