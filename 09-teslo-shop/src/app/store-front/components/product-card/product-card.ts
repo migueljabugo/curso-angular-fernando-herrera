b8759dbbce0675095fc8d7879f826033dcd4ec44
Miguel Angel González <migueljabugo@gmail.com>
2026-01-29 17:30:54 +0100

    249. Mostrar productos en el componente desde el servicio

--- 09-teslo-shop/src/app/store-front/components/product-card/product-card.ts
+++ 09-teslo-shop/src/app/store-front/components/product-card/product-card.ts
@@ -1,10 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Product } from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    SlicePipe
+  ],
   templateUrl: './product-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCard { }
+export class ProductCard {
+
+  product = input.required<Product>();
+  imageUrl = computed(() => {
+    //if (this.product().images.length === 0) {
+    //  return 'https://via.placeholder.com/640x480.png?text=No+image';
+    //}
+    return `http://localhost:3000/api/files/product/${this.product().images[0]}`
+  });
+}
--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
@@ -1,5 +1,5 @@
 
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@store-front/components/product-card/product-card';
 import { rxResource } from '@angular/core/rxjs-interop';
@@ -19,9 +19,8 @@ export class HomePage {
   productResource = rxResource({
     params: () => ({}),
     stream: ({params}) => {
-      return this.productsService.getProducts();
+      return this.productsService.getProducts({});
     }
   });
-
 
  }
