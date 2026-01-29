8a695a9f7ea490b086c53385a2ad0bf0ce50e24a
Miguel Angel González <migueljabugo@gmail.com>
2026-01-29 17:13:54 +0100

    247. Parametrización de la url en una variable de entorno. Y parametrizado de servicio

--- 09-teslo-shop/src/app/products/services/products.service.ts
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -2,14 +2,38 @@ import { inject, Injectable } from '@angular/core';
 import { ProductsResponse } from '../interfaces/product.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+};
 
 @Injectable({providedIn: 'root'})
 export class ProductsService {
 
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>('http://localhost:3000/api/products')
+  getProducts(options: Options= {}): Observable<ProductsResponse> {
+
+    const { limit = 9, offset = 0, gender = '' } = options;
+
+
+
+
+    return this.http.get<ProductsResponse>(`${baseUrl}/products`,
+      {
+        params: {
+          limit,
+          offset,
+          gender
+        }
+      }
+    )
     .pipe(
       tap(response => console.log(response))
     );
