a93635c1b709d53fc5f1351e2d2f65319e00d8cb
Miguel Angel González <migueljabugo@gmail.com>
2026-01-29 18:49:23 +0100

    253. Carga la páginas por genero en la ruta

--- 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.ts
+++ 09-teslo-shop/src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,9 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { ProductCard } from "@store-front/components/product-card/product-card";
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [
+    ProductCard,
+    TitleCasePipe
+  ],
   templateUrl: './gender-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GenderPage { }
+export class GenderPage {
+
+  route = inject(ActivatedRoute);
+
+  gender = toSignal(this.route.params.pipe(
+    map(({gender}) => gender)
+  ));
+
+  productsService = inject(ProductsService);
+
+  productResource = rxResource({
+    params: () => ({gender: this.gender()}),
+    stream: ({params}) => {
+      return this.productsService.getProducts({gender:params.gender});
+    }
+  });
+
+ }
