e148bd46f4229fbf52104a17df3951cd94287ffe
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 00:40:42 +0100

    Se extrae la lógica de la paginación a un servicio para que sea mas reutilizable

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.ts
@@ -2,10 +2,9 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ProductsService } from '@products/services/products.service';
 import { ProductCard } from '@store-front/components/product-card/product-card';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { Pagination } from "@shared//components/pagination/pagination";
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import { PaginationService } from '@shared//components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -19,24 +18,18 @@ import { map } from 'rxjs';
 export class HomePage {
 
   productsService = inject(ProductsService);
-  activatedRoute = inject(ActivatedRoute);
+
+  pagintaionSErvice = inject(PaginationService);
 
 
-  currentPage = toSignal(this.activatedRoute.queryParamMap
-    .pipe(
-      map( (params) => (params.get('page') ? +params.get('page')! : 1)),
-      map( (page) => (isNaN(page) ? 1 : page))
-    ),
-    {
-      initialValue: 1
-    }
-  );
+
+
 
 
 
 
   productResource = rxResource({
-    params: () => ({page: this.currentPage() - 1}),
+    params: () => ({page: this.pagintaionSErvice.currentPage() - 1}),
     stream: ({params}) => {
       return this.productsService.getProducts({
         offset: params.page * 9
