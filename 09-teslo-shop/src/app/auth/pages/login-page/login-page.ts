ebd769def524e91c0d6bcee9427242391987684d
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 18:07:36 +0100

    Formulario de login

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.ts
@@ -1,12 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { single } from 'rxjs';
+
 
 @Component({
   selector: 'app-login-page',
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './login-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPage { }
+export class LoginPage {
+
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPositing = signal(false);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value)
+
+    if(this.loginForm.invalid){
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+
+    console.log(email, password);
+  }
+
+ }
