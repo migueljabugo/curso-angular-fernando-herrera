9e5e5afbc5149bcbf29ce556941f99d06456e0bb
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 21:53:04 +0100

    287. Creación del panel de administración y muestra la información del usuario

--- 09-teslo-shop/src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.ts
+++ 09-teslo-shop/src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-dashboard-layout',
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './admin-dashboard-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminDashboardLayout { }
+export class AdminDashboardLayout {
+
+  authService = inject(AuthService);
+
+  user = computed(() => this.authService.user());
+
+
+  logout() {
+    this.authService.logout();
+  }
+}
