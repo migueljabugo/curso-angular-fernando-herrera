c05e0ae2e04fe3b34e27949fbdd399b793f8c904
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 19:11:02 +0100

    275. Comprobación de si el usuario está logado previamente cuando llega a la web

--- 09-teslo-shop/src/app/auth/services/auth.service.ts
+++ 09-teslo-shop/src/app/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { User } from '@auth/interfaces/user.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
@@ -16,6 +17,13 @@ export class AuthService {
   private _token = signal<string | null>(null);
 
   private http = inject(HttpClient);
+
+
+  //Comprueba que estamos logados nada mas se inicializa el servicio
+  checkStatusResource = rxResource({
+    stream: () => this.checkStatus()
+  });
+
 
   authStatus = computed(() => {
     if (this._authStatus() === 'checking') return 'checking';
@@ -53,5 +61,34 @@ export class AuthService {
     );
   }
 
+  checkStatus(): Observable<boolean>{
+
+    const token = localStorage.getItem('token');
+
+    if(!token){
+      return of(false);
+    }
+
+    return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).pipe(
+      tap(({user, token}) => {
+        this._user.set(user);
+        this._token.set(token);
+        this._authStatus.set('authenticated');
+
+        localStorage.setItem('token', token);
+      }),
+      map(resp => true),
+      catchError((error) => {
+        this._authStatus.set('not-authenticated');
+        this._user.set(null);
+        this._token.set(null);
+        return of(false);
+      })
+    );
+  }
 
 }
