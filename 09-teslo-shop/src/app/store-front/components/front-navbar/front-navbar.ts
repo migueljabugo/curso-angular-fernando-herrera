8864992c9309e8b7f8c0ae36a5c1e9c2ead64fde
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 19:34:56 +0100

    277. Gestion del usuario logado en el home

--- 09-teslo-shop/src/app/store-front/components/front-navbar/front-navbar.ts
+++ 09-teslo-shop/src/app/store-front/components/front-navbar/front-navbar.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'front-navbar',
@@ -10,4 +11,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './front-navbar.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FrontNavbar { }
+export class FrontNavbar {
+
+  authservice = inject(AuthService);
+
+
+
+ }
