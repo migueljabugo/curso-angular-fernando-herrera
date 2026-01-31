b35dddfc5e9842a9fe2760a4bd4130e1a3452f32
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 18:44:16 +0100

    274. Control de error o no al logarse y redirección al home cuando el login es OK

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
@@ -18,6 +18,7 @@ export class LoginPage {
   fb = inject(FormBuilder);
   hasError = signal(false);
   isPositing = signal(false);
+  router = inject(Router);
 
   authService = inject(AuthService);
 
@@ -41,8 +42,16 @@ export class LoginPage {
     const { email = '', password = '' } = this.loginForm.value;
 
     console.log(email, password);
-    this.authService.login(email!, password!).subscribe(resp => {
-      console.log(resp);
+    this.authService.login(email!, password!).subscribe(isAuthenticated => {
+      if (isAuthenticated){
+        this.router.navigateByUrl('/');
+        return;
+      }
+
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
     });
 
   }
--- 09-teslo-shop/src/app/auth/services/auth.service.ts
+++ 09-teslo-shop/src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { User } from '@auth/interfaces/user.interface';
-import { tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
@@ -31,7 +31,7 @@ export class AuthService {
 
   token = computed(() => this._token());
 
-  login(email: string, password: string){
+  login(email: string, password: string):Observable<boolean>{
     return this.http.post<AuthResponse>(`${baseUrl}/auth/login`, {
       email: email,
       password: password
@@ -42,6 +42,13 @@ export class AuthService {
         this._authStatus.set('authenticated');
 
         localStorage.setItem('token', token);
+      }),
+      map(resp => true),
+      catchError((error) => {
+        this._authStatus.set('not-authenticated');
+        this._user.set(null);
+        this._token.set(null);
+        return of(false);
       })
     );
   }
