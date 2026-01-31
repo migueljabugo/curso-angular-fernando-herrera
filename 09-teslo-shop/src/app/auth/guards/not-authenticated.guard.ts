1a5c42bbfb438fa57ab1497ade4d7a6a9b814fef
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 20:48:09 +0100

    280. Impedimos que un usuario entre en la página de login cuando el usuario está logado mediante un GUARD

---
+++ 09-teslo-shop/src/app/auth/guards/not-authenticated.guard.ts
@@ -0,0 +1,24 @@
+import { inject } from '@angular/core';
+import { CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
+import { firstValueFrom } from 'rxjs';
+
+export const NotAuthenticatedGuard: CanMatchFn = async(
+  route: Route,
+  segments: UrlSegment[]
+) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const isAuthenticated = await firstValueFrom(authService.checkStatus());
+
+  console.log(isAuthenticated);
+
+  if(isAuthenticated){
+    router.navigateByUrl('/');
+    return false;
+  }
+
+  return true;
+}
