5c130e113fe5ddc08177cc1e4a63ac7b60a4416b
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 21:58:56 +0100

    Tarea: control de rutas por un usuario con un rol expecífico

--- 09-teslo-shop/src/app/app.routes.ts
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { IsAdminGuard } from '@auth/guards/is-admin.guard';
 import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
@@ -13,6 +14,9 @@ export const routes: Routes = [
     {
       path: 'admin',
       loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
+      canMatch: [
+        IsAdminGuard
+      ]
     },
     {
       path: '',
