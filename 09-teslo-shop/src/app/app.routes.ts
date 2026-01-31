8299d1f74971e6d885cf62a29948cb5a9d3e4c80
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-01-31 01:38:16 +0100

    270. Creación de la estructura, navegación y paginas de auth. Creación del componente de login

--- 09-teslo-shop/src/app/app.routes.ts
+++ 09-teslo-shop/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-
+    {
+      path: 'auth',
+      loadChildren: () => import('./auth/auth.routes')
+      //TODO GUARDS
+    },
     {
       path: '',
       loadChildren: () => import('./store-front/store-front.routes')
     }
+
 
 ];
---
+++ 09-teslo-shop/src/app/auth/pages/register-page/register-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [],
+  templateUrl: './register-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterPage { }
