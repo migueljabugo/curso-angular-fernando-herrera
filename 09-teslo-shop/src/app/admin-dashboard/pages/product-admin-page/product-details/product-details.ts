c0cc45110d5c570375aa7cd487e5e26e860ce3cc
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-01 13:08:00 +0100

    293. Cargando información básica en el formulario en los componentes normales

--- 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,20 +1,53 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit } from '@angular/core';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
   selector: 'product-details',
   imports: [
-    ProductCarousel
+    ProductCarousel,
+    ReactiveFormsModule
   ],
   templateUrl: './product-details.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetails {
-
+export class ProductDetails implements OnInit {
   product = input.required<Product>();
+  fb = inject(FormBuilder);
+
+  productForm = this.fb.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: ['', [Validators.required, Validators.min(0)]],
+    stock: ['', [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+    tags: ['']
+  });
 
   sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>){
+    this.productForm.reset(this.product() as any);
+    //this.productForm.patchValue(formLike as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') });
+  }
+
+
+  onSubmit(){
+    console.log(this.productForm.value);
+
+  }
+
+
 
 
 }
