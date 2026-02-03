dad3f1b1d8868b64736f9bd66c49e3ea9b5d38d8
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-03 17:44:07 +0100

    333. Change detection

--- 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
+++ 11-my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,9 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { Title } from '@shared/title/title';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [
+    Title,
+    JsonPipe
+  ],
   templateUrl: './change-detection.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ChangeDetection { }
+export default class ChangeDetection {
+
+  public currentFramework = computed(() =>
+    `Change detection - ${ this.frameworkAsSignal().name }`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  constructor(){
+
+    setTimeout(() => {
+      //this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update(framework => ({
+        ...framework,
+        name: 'React'
+      }));
+
+
+      console.log("Hecho")
+    }, 3000);
+  }
+
+
+}
