fa3c234bd2ad53b1a011dda1de3b7dab664f2e62
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-03 17:18:14 +0100

    330. Control Flow: FOR

--- 11-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.ts
+++ 11-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.ts
@@ -13,6 +13,8 @@ export default class ControlFlow {
 
   public showContent = signal(false);
   public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik', 'React']);
+  public frameworks2 = signal([]);
 
   public toggleContent() {
     this.showContent.update(val => !val);
