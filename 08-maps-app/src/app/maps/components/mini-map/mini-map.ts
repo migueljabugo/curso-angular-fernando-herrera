ff9654276da2cf3ff90f89cb1740e12df9682fd9
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-04 18:23:44 +0100

    232. Tarea mostrar mapas en el componente y el marcador con la ubicación de la casa

--- 08-maps-app/src/app/maps/components/mini-map/mini-map.ts
+++ 08-maps-app/src/app/maps/components/mini-map/mini-map.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ElementRef, signal, viewChild } from '@angular/core';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
+import { environment } from '../../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapboxKey;
+
 
 @Component({
   selector: 'app-mini-map',
@@ -6,4 +12,34 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './mini-map.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MiniMap { }
+export class MiniMap implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  lngLat = input.required<LngLatLike>();
+
+
+  async ngAfterViewInit() {
+
+    if (!this.divElement()) return;
+
+    const element = this.divElement()?.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat(), // starting position [lng, lat]
+      zoom: 14, // starting zoom
+    });
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const marker = new mapboxgl.Marker({
+      color: color
+    })
+    .setLngLat(this.lngLat())
+    .addTo(map);
+  }
+}
