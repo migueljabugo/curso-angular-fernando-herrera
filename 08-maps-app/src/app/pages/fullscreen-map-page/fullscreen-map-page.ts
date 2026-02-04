d14e29152449e57f0b0ea25a82c0c396430d5d05
ALTEDA\miguelangel.gonzalez <migueljabugo@gmail.com>
2026-02-04 14:53:33 +0100

    225. Control del zoom desde el input y actualización del valor en este

--- 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ 08-maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,6 +1,8 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 import { environment } from '../../../environments/environment';
+import { map } from 'rxjs';
+import { DecimalPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 
@@ -9,20 +11,45 @@ mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './fullscreen-map-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: `
     div {
       width:100vw;
       height: calc(100vh - 64px);
-      background-color: red;
+    }
+
+    #controls {
+      background-color: white;
+      padding:10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 999;
+      box-shadow: 0 0 10px 0 rgba(0,0,0,0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
     }
   `
 })
 export class FullscreenMapPage implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map');
+
+  map = signal<mapboxgl.Map | null>(null);
+
+  zoom = signal(14);
+
+  zoomEffect = effect(() => {
+    if (!this.map()) return;
+
+    //this.map()?.setZoom(this.zoom());
+    this.map()?.zoomTo(this.zoom());
+  });
 
 
   async ngAfterViewInit() {
@@ -37,8 +64,21 @@ export class FullscreenMapPage implements AfterViewInit {
       container: element, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-6.729120964211328, 37.91690866444012], // starting position [lng, lat]
-      zoom: 14, // starting zoom
+      zoom: this.zoom(), // starting zoom
     });
+
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map){
+
+    map.on('zoomend', (e) => {
+      const newZoom = map.getZoom();
+      this.zoom.set(newZoom);
+    });
+
+
+    this.map.set(map);
   }
 
 }
